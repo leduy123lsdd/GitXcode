0085351cc11f0401ec6a7a5a2f5b432e6c9edd3a
duy <duy123galaxy13@gmail.com>
2018-07-29 15:28:05 +0700

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Lê Duy on 7/29/18.
+//  Copyright © 2018 Lê Duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
